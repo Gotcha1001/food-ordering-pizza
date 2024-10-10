f46f9861ae5aeaff277eefc0d295466a77c1774a
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-10-10 14:26:57 +0200

    TEST66

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions"; // Import from the new location
 import { UserInfo } from "@/app/models/UserInfo";
 
 export async function isAdmin() {
